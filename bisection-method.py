b2556c90b219280712cae500f305ad96af37c081
Gabriel Silva <149592906+Gabrielzxd@users.noreply.github.com>
2025-10-26 15:06:42 -0400

    update bisection-method.py

--- bisection-method.py
+++ bisection-method.py
@@ -4,6 +4,8 @@ def function(x):
 
 # Implementation using recursion
 def bisection_method_recursive(function, a, b, error):
+    if (function(a) * function(b) >= 0):
+        return -999999999
     x = (a + b) / 2
 
     if abs(function(x)) < error:
@@ -12,22 +14,22 @@ def bisection_method_recursive(function, a, b, error):
     if function(a)*function(x) < 0:
         return bisection_method_recursive(function, a, x, error)
     
-    elif function(x)*function(b) < 0:
+    if function(x)*function(b) < 0:
         return bisection_method_recursive(function, x, b, error)
-    
-    else:
-        return -999999999
+
 
 # Similar to binary search
 def bisection_method_iterative(function, a, b, error):
-    x = (a + b) / 2
-    while abs(function(x)) > error:
-        if function(a) * function(x) < 0:
-            b = x
-        if function(x) * function(b) < 0:
-            a = x
+    if(function(a) * function(b) < 0):
         x = (a + b) / 2
-    return x
+        while abs(function(x)) > error:
+            if function(a) * function(x) < 0:
+                b = x
+            if function(x) * function(b) < 0:
+                a = x
+            x = (a + b) / 2
+        return x
+    return -999999999
 
 # Test
 x = bisection_method_recursive(function, 1, 3, 0.01)
